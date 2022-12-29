b5280abd6ef2b68a084bc539ce3ebee4bcd215d9
Lizvan27 <lizvanmandy@gmail.com>
2022-12-29 14:12:59 -0500

    agregacion funcion lanzar

--- src/app/elementos/dado/dado.component.ts
+++ src/app/elementos/dado/dado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-dado',
@@ -6,7 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./dado.component.css']
 })
 export class DadoComponent {
+  @ViewChild('reset') reset!: any;
+  numero: number = 0;
+  aleatorio = 0;
+  rei: boolean = false
   
 
+  lanzar(){
+    this.aleatorio = Math.round(Math.random()*6);
+  }
+  reiniciar(){
+    this.reset.nativeElement.value = this.lanzar
+  }
 
 }
